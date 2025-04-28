8a45ddb2dd5249e7623113070d34a8ccf298102e
NoFoster <kfoster0@chicagobooth.edu>
2025-04-28 18:16:17 +0000

    fixing routing of new_payment and do_payment

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -23,11 +23,37 @@ class CalculationsController  < ApplicationController
     def do_square_root
       input_str = params.fetch("number")
       @input_num = input_str.to_f
-      @result = input_num ** 0.5
+      @result = @input_num ** 0.5
       @input_num = input_str.to_i
       render({ :template => "calc_templates/square_root_results"})
     end
 
+    def new_payment
+        render({ :template => "calc_templates/new_payment"})
+    end
+
+    def do_payment
+      @user_apr = params.fetch("user_apr").to_f
+      @user_years = params.fetch("user_years").to_f
+      @user_principal = params.fetch("user_principal").to_f
+    
+      @numerator = (@user_apr / (100*12) ) * @user_principal
+      @denominator = 1 - (1 + @user_apr/(100*12)) ** (-@user_years * 12)
+      @the_result = @numerator / @denominator
+      render({ :template => "calc_templates/payment_results"})
+    end 
+
+    def new_random
+      render({ :template => "calc_templates/new_random"})
+    end
+
+    def do_random
+      @user_min = params.fetch("user_min").to_f
+      @user_max = params.fetch("user_max").to_f
+    
+      @the_result= rand(@user_min..@user_max)
+      render({ :template => "calc_templates/random_results"})
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   
-  ##get("/", { :controller => "calculations", :action => "homepage"})
+  get("/", { :controller => "calculations", :action => "new_square"})
 
   get("/square/new", { :controller => "calculations", :action => "new_square"})
   
   get("/square/results", { :controller => "calculations", :action => "do_square"})
 
   get("/square_root/new", { :controller => "calculations", :action => "new_square_root"})
+
+  get("square_root/results", { :controller => "calculations", :action => "do_square_root"})
+
+  get("/payment/new", { :controller => "calculations", :action => "new_payment"})
+
+  get("/payment/results", { :controller => "calculations", :action => "do_payment"})
+
+  get("/random/new", { :controller => "calculations", :action => "new_random"})
+
+  get("/random/results", { :controller => "calculations", :action => "do_random"})
 end
