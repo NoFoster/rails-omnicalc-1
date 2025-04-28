1a40f64d76b6a41d8e526a948de950d2777ae7a9
NoFoster <kfoster0@chicagobooth.edu>
2025-04-28 17:27:57 +0000

    Square Results

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,17 +5,31 @@ class CalculationsController  < ApplicationController
     end
 
     def new_square
-        render({ :template => "calculations/new_square"})
+        render({ :template => "calc_templates/new_square"})
     end
 
     def do_square
       input_str = params.fetch("number")
-      input_num = input_str.to_f
-      @result = input_num * input_num
-      render({ :template => "calculations/square_results"})
+      @input_num = input_str.to_f
+      @result = @input_num * @input_num
+      @input_num = input_str.to_i
+      render({ :template => "calc_templates/square_results"})
     end
 
+    def new_square_root
+      render({ :template => "calc_templates/new_square_root"})
+    end
+
+    def do_square_root
+      input_str = params.fetch("number")
+      @input_num = input_str.to_f
+      @result = input_num ** 0.5
+      @input_num = input_str.to_i
+      render({ :template => "calc_templates/square_root_results"})
+    end
+
+
   end
-  
+
   
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  get("/", { :controller => "calculations", :action => "homepage"})
+  ##get("/", { :controller => "calculations", :action => "homepage"})
 
   get("/square/new", { :controller => "calculations", :action => "new_square"})
   
-  get("/square", { :controller => "calculations", :action => "do_square"})
+  get("/square/results", { :controller => "calculations", :action => "do_square"})
+
+  get("/square_root/new", { :controller => "calculations", :action => "new_square_root"})
 end
